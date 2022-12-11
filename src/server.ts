7d8a04f803e700b87a5982005d0bf95df73bd8e1
JamiuKolawole04 <Jakko4real@gmail.com>
2022-12-11 08:29:43 +0100

    chore: adding allowed origins

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,11 @@ app.use(express.json({ limit: "10mb" }));
 app.use(
   express.urlencoded({ extended: false, limit: "50mb", parameterLimit: 50000 })
 );
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://hotel-ranking-app.netlify.app"],
+  })
+);
 app.use(
   fileUpload({
     useTempFiles: true,
